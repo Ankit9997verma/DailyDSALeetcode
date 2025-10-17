796a65b074773068bcf1ecc91774e1fb45449beb
Ankit9997verma <ankit9997verma@gmail.com>
2025-10-17 08:59:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (78.55%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.java
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -5,8 +5,8 @@ class Solution {
         int count = 0;
         int curr = 0;
 
-        for (int i = 2; i < n; i++) {
-            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+        for (int i = 1; i < n-1; i++) {
+            if (nums[i] - nums[i-1] == nums[i+1] - nums[i]) {
                 curr += 1;     
                 count += curr; 
             } else {
