cba919c15542d793539bb2a51db450a755d10498
Ankit9997verma <ankit9997verma@gmail.com>
2025-11-14 23:51:12 +0530

    Time: 4 ms (92.32%) | Memory: 44.9 MB (16.52%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        String [] stp=path.split("/");
+        for( String str:stp){
+            if(str.isEmpty() || str.equals(".")){
+                continue;
+          }else if(str.equals("..")){
+                     if(!st.isEmpty()){
+                          st.pop();
+                      }
+            }else{
+                    st.push(str);
+                }
+        }
+        if(st.isEmpty()){
+            return "/";
+        }
+   
+            
+        StringBuilder sb=new StringBuilder();
+        for( String stt:st){
+            sb.append("/").append(stt);
+
+        }
+        return sb.toString();
+
+       
+    }
+}
