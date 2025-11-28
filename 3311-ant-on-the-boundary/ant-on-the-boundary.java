c2ffbbef962df2e94514a37f2d5db55e0e306af4
Ankit9997verma <ankit9997verma@gmail.com>
2025-11-28 21:24:45 +0530

    Time: 1 ms (11.90%) | Memory: 43.7 MB (33.64%) - LeetSync

--- 3311-ant-on-the-boundary/ant-on-the-boundary.java
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.java
@@ -1,16 +1,11 @@
 class Solution {
     public int returnToBoundaryCount(int[] nums) {
-        int[] prefix= new int[nums.length];
-        int count =0 ;
-        prefix[0]=nums[0];
-        for(int i =1 ; i<nums.length; i++){
-            prefix[i]=prefix[i-1]+nums[i];
-        }
-        for(int j=0 ; j<nums.length ; j++){
-            if(prefix[j]==0){
-                count = count+1 ;
-            }
-        }
-        return count ; 
+        int count = 0;
+        int ans = 0;
+        for(int i=0;i<nums.length;i++){
+            ans+=nums[i];
+            if(ans == 0 ) count++;
+        } 
+        return count;
     }
 }
