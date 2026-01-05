7f930c99df034b98de96f16c4028ba87df18d75e
Ankit9997verma <ankit9997verma@gmail.com>
2026-01-06 01:05:28 +0530

    Time: 10 ms (72.82%) | Memory: 46.4 MB (70.13%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -5,14 +5,20 @@ class Solution {
         for(int i =0 ; i< nums.length ; i++){
              int currentcount=0;
             int currentsum=0;
-            for(int j =1 ; j<= nums[i] ; j++){
-              
+            for(int j =1 ; j*j <= nums[i] ; j++){
                 if(nums[i]%j==0){
-                     currentcount=currentcount+1;
-                     currentsum=currentsum+j;
-                     
-                }
+                    if(j==nums[i]/j){
+                    currentcount+=1;
+                    currentsum+=j;
+                    }
+                    else{
+                        currentcount+=2;
+                        currentsum+=j;
+                        currentsum+= (nums[i]/j);
+                    }
+                
                 if(currentcount>4) break;
+                }
                 
             }
             if(currentcount==4){
