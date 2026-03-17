886f9cf4252cb44385fddba5311742867dbe315c
Ankit Verma <ankit9997verma@gmail.com>
2026-03-17 16:31:39 +0530

    Time: 2 ms (70.43%) | Memory: 47.6 MB (44.74%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,28 +1,26 @@
 class Solution {
-    public void helper(String s, ArrayList<String> list, StringBuilder sb) {
-        if (s.length() == 0) {
+    public void helper(String s , int i , List<String> list , StringBuilder sb){
+        if(i == s.length()){
             list.add(sb.toString());
             return;
         }
-        char ch = s.charAt(0);
-        String remaining = s.substring(1);
-        if (Character.isLetter(ch)) {
-            StringBuilder op1 = new StringBuilder(sb);
-            op1.append(Character.toLowerCase(ch));
-            StringBuilder op2 = new StringBuilder(sb);
-            op2.append(Character.toUpperCase(ch));
-
-            helper(remaining, list, op1);
-            helper(remaining, list, op2);
-        } else {
-            StringBuilder op = new StringBuilder(sb);
-            op.append(ch);
-            helper(remaining, list, op);
+        char ch = s.charAt(i);
+        int len = sb.length();
+        if(Character.isLetter(ch)){
+           
+            helper(s,i+1,list,sb.append(Character.toLowerCase(ch)));
+            sb.setLength(len);
+            
+            helper(s,i+1, list ,sb.append(Character.toUpperCase(ch)));
+            sb.setLength(len);
+        }else {
+            helper(s,i+1 ,list , sb.append(ch));
+            sb.setLength(len);
         }
     }
     public List<String> letterCasePermutation(String s) {
-        ArrayList<String> list = new ArrayList<>();
-        helper(s, list, new StringBuilder());
-        return list;
+        List<String> list = new ArrayList<>();
+        helper(s , 0 , list , new StringBuilder());
+        return list ;
     }
 }
