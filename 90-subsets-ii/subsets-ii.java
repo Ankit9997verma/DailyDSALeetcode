4821f7f4d4fbc7fec88cd0ef65b940666a19924a
Ankit Verma <ankit9997verma@gmail.com>
2026-03-17 12:16:07 +0530

    Time: 3 ms (56.25%) | Memory: 45.3 MB (38.29%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,20 +1,22 @@
 class Solution {
-    public void helper(int nums[], List<List<Integer>> ans , List<Integer> curr , int i){
-           ans.add(new ArrayList<> (curr));
-           for(int j=i; j<nums.length ; j++){
-             if(j>i && nums[j]==nums[j-1]) continue;
-             curr.add(nums[j]);
-             helper(nums,ans , curr, j+1);
-             curr.remove(curr.size()-1);
-           }
-            
-
+    public void solve(int[] nums , int i , HashSet<List<Integer>> set , List<Integer>list){
+        if(i == nums.length){
+             List<Integer> temp = new ArrayList<>(list);
+            Collections.sort(temp);
+            set.add(temp);
+            return;
+        }
+        list.add(nums[i]);
+        solve(nums , i+1 , set , list);
+        list.remove(list.size()-1);
+        solve(nums , i+1 , set , list);
     }
+    
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> curr= new ArrayList<>();
-        helper(nums, ans , curr , 0);
-        return ans;
+        
+        HashSet<List<Integer>> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        solve(nums , 0 , set , list);
+        return new ArrayList<>(set);
     }
 }
