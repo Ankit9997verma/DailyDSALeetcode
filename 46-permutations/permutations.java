a48a966edfe1b895b087f62dca5e8cff03482c9f
Ankit Verma <ankit9997verma@gmail.com>
2026-03-19 00:44:07 +0530

    Time: 1 ms (85.16%) | Memory: 46.3 MB (8.66%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,35 +1,74 @@
+// class Solution {
+//     public void helper(int[] nums, ArrayList<ArrayList<Integer>> list, ArrayList<Integer> ans) {
+//         if (nums.length == 0) {
+//             list.add(new ArrayList<>(ans));
+//             return;
+//         }
+
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (set.contains(nums[i])) continue;
+//             set.add(nums[i]);
+//             int[] remaining = new int[nums.length - 1];
+//             int k = 0;
+//             for (int j = 0; j < nums.length; j++) {
+//                 if (j != i) {
+//                     remaining[k++] = nums[j];
+//                 }
+//             }
+
+//             ArrayList<Integer> newAns = new ArrayList<>(ans);
+//             newAns.add(nums[i]);
+
+//             helper(remaining, list, newAns);
+//         }
+//     }
+
+//     public List<List<Integer>> permute(int[] nums) {
+//         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+//         helper(nums, list, new ArrayList<>());
+//         return new ArrayList<>(list);
+//     }
+// }
+
+//above approach is of recursion 
 class Solution {
-    public void helper(int[] nums, ArrayList<ArrayList<Integer>> list, ArrayList<Integer> ans) {
-        if (nums.length == 0) {
-            list.add(new ArrayList<>(ans));
+    public void helper(int[] nums, List<List<Integer>> list, int index) {
+        if (index == nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for (int num : nums) temp.add(num);
+            list.add(temp);
             return;
         }
 
         HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = index; i < nums.length; i++) {
+            // avoid duplicates
             if (set.contains(nums[i])) continue;
             set.add(nums[i]);
 
-            // create remaining array
-            int[] remaining = new int[nums.length - 1];
-            int k = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (j != i) {
-                    remaining[k++] = nums[j];
-                }
-            }
+            // choose (swap)
+            swap(nums, i, index);
 
-            ArrayList<Integer> newAns = new ArrayList<>(ans);
-            newAns.add(nums[i]);
+            // explore
+            helper(nums, list, index + 1);
 
-            helper(remaining, list, newAns);
+            // undo (backtrack)
+            swap(nums, i, index);
         }
     }
 
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public List<List<Integer>> permute(int[] nums) {
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        helper(nums, list, new ArrayList<>());
-        return new ArrayList<>(list);
+        List<List<Integer>> list = new ArrayList<>();
+        helper(nums, list, 0);
+        return list;
     }
 }
