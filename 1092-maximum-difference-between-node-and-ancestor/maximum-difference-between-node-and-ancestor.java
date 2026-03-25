ab9c5fbde99adb7fbf49d0857eb1cc8a25f599ab
Ankit Verma <ankit9997verma@gmail.com>
2026-03-25 12:44:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (63.98%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -13,20 +13,39 @@
  *     }
  * }
  */
-class Solution {
-    int max =0 ;
-    public int maxAncestorDiff(TreeNode root) {
-        if(root == null) return 0;
-        helper(root , root.val);
-        maxAncestorDiff(root.left );
-        maxAncestorDiff(root.right);
-        return max; 
-    }
-    public void helper(TreeNode root , int val){
-        if(root == null) return ;
-        max = Math.max(max , Math.abs(val-root.val));
-        helper(root.left , val);
-        helper(root.right , val);
 
+
+ // class Solution {
+//     int max =0 ;
+//     public int maxAncestorDiff(TreeNode root) {
+//         if(root == null) return 0;
+//         helper(root , root.val);
+//         maxAncestorDiff(root.left );
+//         maxAncestorDiff(root.right);
+//         return max; 
+//     }
+//     public void helper(TreeNode root , int val){
+//         if(root == null) return ;
+//         max = Math.max(max , Math.abs(val-root.val));
+//         helper(root.left , val);
+//         helper(root.right , val);
+
+//     }
+// }
+class Solution {
+    int ans =0 ;
+    public void helper(TreeNode root , int max , int min ){
+        if(root == null) return ;
+        max = Math.max(max , root.val);
+         min=Math.min(min , root.val);
+        ans = Math.max(ans  , Math.abs(max-min));
+        helper(root.left , max, min);
+        helper(root.right , max, min);
+
+    }
+    public int maxAncestorDiff(TreeNode root) {
+        if(root == null) return 0 ; 
+        helper(root , root.val , root.val );
+        return ans;
     }
 }
