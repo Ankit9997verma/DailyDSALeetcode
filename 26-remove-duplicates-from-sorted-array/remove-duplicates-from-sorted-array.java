ca245a4b428e682fedcdb2bde192f45f9db64eff
Ankit Verma <ankit9997verma@gmail.com>
2026-04-29 02:36:01 +0530

    Time: 4 ms (5.63%) | Memory: 46.7 MB (42.42%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> set = new LinkedHashSet<>();
+
+        for(int num : nums) {
+            set.add(num);
+        }
+
+        int i = 0;
+        for(int val : set) {
+            nums[i++] = val;
+        }
+
+        return set.size();
+    }
+}
