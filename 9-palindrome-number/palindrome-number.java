7616bbda8d659aa85b2852fbadff3a34281ad46b
Ankit Verma <ankit9997verma@gmail.com>
2026-04-29 01:51:04 +0530

    Time: 5 ms (79.51%) | Memory: 45.9 MB (72.68%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
-
+        if(x < 0) return false ;
         int original = x;
-        int ans = 0;
-
-        while (x != 0) {
+        int reverse = 0;
+        while(x > 0){
             int digit = x % 10;
+            reverse = reverse * 10 + digit;
             x = x / 10;
-            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && digit > 7))
-                return false;
-
-            ans = ans * 10 + digit;
         }
-
-        return original == ans;
+        return reverse == original ;
     }
 }
