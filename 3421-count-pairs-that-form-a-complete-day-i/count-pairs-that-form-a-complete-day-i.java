95bfef7c8fa67bdc083eac0a823ffd91cafa7531
Ankit Verma <ankit9997verma@gmail.com>
2026-04-29 21:59:51 +0530

    Time: 2 ms (13.04%) | Memory: 44.3 MB (37.68%) - LeetSync

--- 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.java
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.java
@@ -1,13 +1,16 @@
 class Solution {
     public int countCompleteDayPairs(int[] hours) {
+        HashMap<Integer , Integer> map = new HashMap<>();
         int count =0 ; 
-        for(int i =0 ; i< hours.length ; i++){
-            for(int j = i+1 ; j< hours.length ; j++){
-                if((hours[i]+hours[j])%24 ==0){
-                    count ++;
-                }
+        for(int h : hours){
+            int rem = h % 24 ; 
+            int comp = (24 - rem) % 24;
 
+            if(map.containsKey(comp)){
+                count+= map.get(comp);
             }
+            map.put(rem , map.getOrDefault(rem , 0) +1);
+
         }
         return count ; 
     }
