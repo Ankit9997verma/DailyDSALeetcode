2dd6178d9a9451e9c909aa9dca62a75c01d4649c
Ankit Verma <ankit9997verma@gmail.com>
2026-04-29 14:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (16.21%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,14 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        while(head != null){
-            if(set.contains(head)){
-                return true ;
-            }
-            set.add(head);
-            head = head.next;
+       if(head == null) return false ;
+       ListNode slow = head ;
+       ListNode fast = head ;
+
+       while(fast!= null && fast.next!=null){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow == fast){
+            return true ;
         }
-        return false ; 
+       }
+       return false ; 
     }
 }
