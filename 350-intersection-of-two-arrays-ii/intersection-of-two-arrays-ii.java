d62cec7f11c8e2879bb9e9bc7ac926f0667ae1a0
Ankit Verma <ankit9997verma@gmail.com>
2026-06-30 00:32:09 +0530

    Time: 7 ms (14.70%) | Memory: 45.2 MB (22.24%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,43 +1,26 @@
 class Solution {
-    public boolean BinarySearch(int target  ,int[] nums1 ){
-        int start = 0 ; 
-        int end  = nums1.length -1 ;
-
-        while(start <= end){
-            int mid = start +(end - start)/2 ;
-            if(nums1[mid]==target){
-                return true ;
-            }
-            else if(nums1[mid]> target){
-                end = mid -1 ;
-            }else {
-                start = mid+1 ;
-            }
-        }
-        return false ;
-    }
     public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
-       
+        Arrays.sort(nums2);
+
         ArrayList<Integer> ans = new ArrayList<>();
+        int i =0 , j =0 ; 
 
-        HashMap<Integer , Integer> map = new HashMap<>();
-        for(int x  : nums1){
-            map.put(x , map.getOrDefault(x,0)+1);
-        }
-        for (int x : nums2) {
-
-            if (BinarySearch(x, nums1) && map.getOrDefault(x, 0) > 0) {
-                ans.add(x);
-                map.put(x, map.get(x) - 1);
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i]==nums2[j]){
+                ans.add(nums1[i]);
+                i++;
+                j++;
+            }else if(nums1[i]< nums2[j]){
+                i++;
+            }else{
+                j++;
             }
         }
-        
-        int size = ans.size();
-        int[] answer = new int[size];
-        for(int i =0 ; i< size ; i++){
-            answer[i]= ans.get(i);
+        int[] res = new int[ans.size()];
+        for(int k =0 ; k< ans.size() ; k++){
+            res[k]=ans.get(k);
         }
-        return answer ; 
+        return res ; 
     }
 }
