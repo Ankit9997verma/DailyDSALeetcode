4047300087dff6690b782f4f3e8d05a944735b4b
Ankit Verma <ankit9997verma@gmail.com>
2026-08-25 10:31:09 +0530

    Time: 26 ms (25.96%) | Memory: 44.9 MB (29.51%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,26 +1,55 @@
 class Solution {
-    public boolean checkInclusion(String s, String s2) {
-        if(s2.length()< s.length()) return false ;
-       int [] freq = new int[128];
-       int[] freq1= new int[128];
-       for(char ch : s.toCharArray()){
-        freq[ch]++;
-       }
-       
-       
-       int m = s.length();
-       int n = s2.length();
-      
-       for(int i =0 ; i< m ; i++){
-          freq1[s2.charAt(i)]++;
-          
-       }
-       if(Arrays.equals(freq1,freq)) return true;
-       for(int i =m ; i<n ; i++){
-         freq1[s2.charAt(i)]++;
-         freq1[s2.charAt(i-m)]--;
-         if(Arrays.equals(freq1, freq)) return true ;
-       }
-       return false ;
+    public boolean checkInclusion(String s1, String s2) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s1.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int i = 0;
+        int j = 0;
+        int required = s1.length();
+
+        while (j < s2.length()) {
+
+            char ch = s2.charAt(j);
+
+            
+            if (map.containsKey(ch)) {
+
+                if (map.get(ch) > 0) {
+                    required--;
+                }
+
+                map.put(ch, map.get(ch) - 1);
+            }
+
+         
+            if (j - i + 1 == s1.length()) {
+
+                if (required == 0) {
+                    return true;
+                }
+
+            
+                char left = s2.charAt(i);
+
+                if (map.containsKey(left)) {
+
+                    if (map.get(left) >= 0) {
+                        required++;
+                    }
+
+                    map.put(left, map.get(left) + 1);
+                }
+
+                i++;
+            }
+
+            j++;
+        }
+
+        return false;
     }
 }
